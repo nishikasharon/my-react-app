5a3d74b819a2d43eb41b9fcb0e94801659c2f8e2
Nishika <nishikasharon@gmail.com>
2026-03-16 16:56:30 +0530

    onclick event covered

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import Lists from './List.jsx'
+import Button from "./Button.jsx"
+
+//click event - An interaction when a user click on a event , we can respond to clicks by passing a callback to he onClick event handler
+
 
 function App(){
-  return(<Lists/>);
+  return(
+  <Button/>
+  );
 }
 export default App
---
+++ src/Button.jsx
@@ -0,0 +1,14 @@
+
+function Button(){
+    
+
+    const handleClick = (e) => e.target.textContent ="OUCHH";
+
+   
+
+    return (
+        <button onClick={(e) => handleClick(e)}>Click me</button>
+    );
+}
+
+export default Button
