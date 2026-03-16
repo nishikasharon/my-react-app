e9c7167ada7a48fff4fcd2ab44dc2c8a81dacd24
Nishika <nishikasharon@gmail.com>
2026-03-16 14:58:22 +0530

    Conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
 
+import UserGreeting from './UserGreeting.jsx';
 
-import Student from './Student.jsx'
 function App(){
   return(
     <>
-    <Student name="Sponegbob" 
-    age = {30}
-    isStudent = {true}/>
-
-    <Student name="Patrick" 
-    age = {40}
-    isStudent = {false}/>
-
-    <Student name="Jessica" 
-    age = {50}
-    isStudent = {false}/>
-
-    <Student name="Sandy" 
-    age = {28}
-    isStudent = {true}/>
-
-    <Student/>
-
+    <UserGreeting 
+    isLoggedIn = {true}
+    />
     </>
   );
 }
