41eb01f8ec90aef3024bcff200dffe8b48f71ae8
Nishika <nishikasharon@gmail.com>
2026-03-16 15:44:18 +0530

    Rendering lists

--- src/List.jsx
+++ src/List.jsx
@@ -1,8 +1,26 @@
 
 function List(){
-    const fruits = ["apple","orange","bananana","coconut","pineapple"]
+    const fruits = [{id: 1, name: "apple", calories: 95},
+                    {id: 2,name: "orange", calories: 45},
+                    {id: 3,name:"bananana", calories: 105},
+                    {id: 4,name: "coconut", calories: 159},
+                    {id: 5,name: "pineapple", calories: 37}]
+//ALPHABETICAL
+    // fruits.sort((a,b) => a.name.localeCompare(b.name));
+ //REVERSE ALPHABETICAL   
+    // fruits.sort((a,b) => b.name.localeCompare(a.name));
+//NUMERIC 
+    // fruits.sort((a,b) => a.calories-b.calories);
+//REVERSE NUMERIC 
+    // fruits.sort((a,b) => b.calories-a.calories);
 
-    const listItems = fruits.map(fruit => <li>{fruit}</li>);
+
+    const lowCalFruit = fruits.filter(fruit => fruit.calories<100);
+
+    const listItems = lowCalFruit.map(lowCalFruit => 
+                                <li key ={lowCalFruit.id}>
+                                {lowCalFruit.name}: &nbsp;
+                                <b>{lowCalFruit.calories}</b></li>);
 
     return(<ol>{listItems}</ol>);
 }
